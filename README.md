4d28b0418c9cbdc2b260534c64d7628e60d65411
Sap <greyl33t@gmail.com>
2019-09-10 21:26:43 +0300

    First version
    Working version release

---
+++ README.md
@@ -0,0 +1,60 @@
+# cleverbot-with-sessions
+
+A node.js client for talking to cleverbot.
+It allows to manage sessions and change bot's characteristics (tweaking).
+
+## Installation:
+
+Installation via `npm`:
+
+     npm install cleverbot-with-sessions
+
+##First time initiation:
+
+To use cleverbot, you need an API key, which you can obtain here: https://www.cleverbot.com/api/my-account
+
+```javascript
+    const cleverbotWithSessions = require('cleverbot-node');
+    
+    let bot = new cleverbotWithSessions("YOUR-API-KEY")
+```
+
+##Usage
+
+###Ask request:
+
+Param 1: Input message   
+Param 2: cs (optional) - previously obtained cleverbot state (session) to continue from - leave blank if new conversation
+
+```javascript
+bot.ask("Hello", cs).then(response => {
+    console.log(response)
+})
+```
+###Response:
+
+```javascript
+{ cs:
+   'MXYxCTh2MQlBdldYQ1BNM0U3MzUJMUZ2MTU2ODExMDQzNwk2NHZIZWxsby4JNjRpV2hhdCdzIHlvdXIgbmFtZT8J',
+  ouput: "What's your name?" }
+```
+
+cs - this parameter stands for “cleverbot state”. It is the encoded state of the conversation so far and includes the whole conversation history up to that point. (Store it somewhere to return to this particular session, it changes after each request)
+
+
+###Configuring tweaking
+
+Param 1: value 0-100 (sensible to wacky)  
+Param 2: value 0-100 (shy to talkative)  
+Param 3:  value 0-100 (self-centred to attentive)  
+
+*Pass `-1` if you want to reset it to default
+
+```javascript
+bot.setTweak(-1, 0, 90)
+```
+
+To fully enjoy sessions - store `cs` and `tweaking` values.
+Remember to `setTweak()` before each time you change session, unless you want to use the same tweak settings.
+
+>Made by Sap#7777
---
+++ index.js
@@ -0,0 +1,75 @@
+/**
+ * Created by Sap.
+ */
+let axios = require('axios');
+let baseURL = `https://www.cleverbot.com/getreply?`
+/**
+ * Creates a new Cleverbot instance
+ * @class
+ */
+class Cleverbot {
+    /**
+     * @param {string} apiKey - The cleverbot.com api key from https://www.cleverbot.com/api/my-account
+     */
+    constructor(apiKey) {
+        if (!apiKey) throw new Error('You need to provide an API Key from https://www.cleverbot.com/api/my-account');
+        this.apiKey = apiKey
+        this.tweak1 = "";
+        this.tweak2 = "";
+        this.tweak3 = "";
+    }
+
+    /**
+     * Sets the session id
+     * @param {object} tweaks - The session id to use
+     */
+
+     //Tweak 1 varies from sensible to wacky
+     //Tweak 2 from shy to talkative
+     //Tweak 3 from self-centred to attentive
+    setTweak(tweak1, tweak2, tweak3) {
+        if(isNaN(tweak1) || isNaN(tweak2) || isNaN(tweak3 )) throw new Error("Tweak values must be numbers")
+        if (tweak1 > 100 && tweak1 < -1 || tweak2 > 100 && tweak2 < -1|| tweak3 > 100 && tweak3 < -1) throw new Error("Tweak values must be between 0 and 100")
+        if(tweak1 != -1) this.tweak1 = tweak1
+        else this.tweak1 = "";
+        if(tweak2 != -1) this.tweak2 = tweak2
+        else this.tweak2 = "";
+        if(tweak3 != -1) this.tweak3 = tweak3
+        else this.tweak3 = "";
+    }
+
+    /**
+     * Sends a question to the api
+     * @param input - the input message
+     * @param sesssion - the input message
+     * @return {Promise}
+     */
+    ask(input, cs = "") {
+        return new Promise((resolve, reject) => {
+            if (!input) reject('No input given');
+            axios.get(baseURL, {
+                params: {
+                    key: this.apiKey,
+                    input : input,
+                    cs : cs,
+                    cb_settings_tweak1 : this.tweak1,
+                    cb_settings_tweak2 : this.tweak2,
+                    cb_settings_tweak3 : this.tweak3
+
+
+                }
+              })
+              .then(function (response) {
+                  let resObj = {
+                      "cs" : response.data.cs,
+                      "ouput" : response.data.output
+                  }
+                  resolve(resObj)
+              })
+              .catch(function (error) {
+                reject(console.log(error));
+              })
+        });
+    }
+}
+module.exports = Cleverbot;
